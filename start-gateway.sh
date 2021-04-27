ef140eeefe025224a1ef527a7e6d91a6ad1a7332
Mark Deutsch <mark.deutsch@duke.edu>
2021-04-27 14:47:03 -0400

    startup scripts now remove mods before adding

--- start-gateway.sh
+++ start-gateway.sh
@@ -1,12 +1,13 @@
 echo "Creating Tenant..."
 
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d @okapi-tenant.json http://localhost:9130/_/proxy/tenants
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @okapi-tenant.json http://localhost:9130/_/proxy/tenants
 
-echo "Adding and enabling mod-apollo-federation-gateway..."
+echo "Removing, adding and enabling mod-apollo-federation-gateway..."
 
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d @mod-apollo-federation-gateway/ModuleDescriptor.json  http://localhost:9130/_/proxy/modules
+curl -X DELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-apollo-federation-gateway-1.0.0
+curl -X DELETE http://localhost:9130/_/discovery/modules/mod-apollo-federation-gateway-1.0.0/a
+curl -X DELETE http://localhost:9130/_/proxy/modules/mod-apollo-federation-gateway-1.0.0
 
-curl -w '\n' -D - -s -X POST -H 'Content-type: application/json' -d @mod-apollo-federation-gateway/DeploymentDescriptor.json http://localhost:9130/_/discovery/modules
-
-curl -w '\n' -X DELETE -D - http://localhost:9130/_/proxy/tenants/diku/modules/mod-apollo-federation-gateway-1.0.0
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d '{"id": "mod-apollo-federation-gateway-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-apollo-federation-gateway/ModuleDescriptor.json  http://localhost:9130/_/proxy/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-apollo-federation-gateway/DeploymentDescriptor.json http://localhost:9130/_/discovery/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d '{"id": "mod-apollo-federation-gateway-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
--- start-services.sh
+++ start-services.sh
@@ -1,21 +1,22 @@
 echo "Creating Tenant..."
 
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d @okapi-tenant.json http://localhost:9130/_/proxy/tenants
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @okapi-tenant.json http://localhost:9130/_/proxy/tenants
 
-echo "Adding and enabling mod-users-graphql..."
+echo "Removing, adding and enabling mod-users-graphql..."
+curl -X DELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-users-graphql-1.0.0
+curl -X DELETE http://localhost:9130/_/discovery/modules/mod-users-graphql-1.0.0/c
+curl -X DELETE http://localhost:9130/_/proxy/modules/mod-users-graphql-1.0.0
 
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d @mod-users/ModuleDescriptor-graphql.json  http://localhost:9130/_/proxy/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-users/ModuleDescriptor-graphql.json  http://localhost:9130/_/proxy/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-users/DeploymentDescriptor-graphql.json http://localhost:9130/_/discovery/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d '{"id": "mod-users-graphql-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
 
-curl -w '\n' -D - -s -X POST -H 'Content-type: application/json' -d @mod-users/DeploymentDescriptor-graphql.json http://localhost:9130/_/discovery/modules
+echo "Removing, adding and enabling mod-permissions-graphql..."
 
-curl -w '\n' -X DELETE -D - http://localhost:9130/_/proxy/tenants/diku/modules/mod-users-graphql-1.0.0
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d '{"id": "mod-users-graphql-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
+curl -X DELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-permissions-graphql-1.0.0
+curl -X DELETE http://localhost:9130/_/discovery/modules/mod-permissions-graphql-1.0.0/b
+curl -X DELETE http://localhost:9130/_/proxy/modules/mod-permissions-graphql-1.0.0
 
-echo "Adding and enabling mod-permissions-graphql..."
-
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d @mod-permissions/ModuleDescriptor-graphql.json  http://localhost:9130/_/proxy/modules
-
-curl -w '\n' -D - -s -X POST -H 'Content-type: application/json' -d @mod-permissions/DeploymentDescriptor-graphql.json http://localhost:9130/_/discovery/modules
-
-curl -w '\n' -X DELETE -D - http://localhost:9130/_/proxy/tenants/diku/modules/mod-permissions-graphql-1.0.0
-curl -w '\n' -X POST -D - -H 'Content-type: application/json' -d '{"id": "mod-permissions-graphql-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-permissions/ModuleDescriptor-graphql.json  http://localhost:9130/_/proxy/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d @mod-permissions/DeploymentDescriptor-graphql.json http://localhost:9130/_/discovery/modules
+curl -X POST -w '\n' -H 'Content-type: application/json' -d '{"id": "mod-permissions-graphql-1.0.0"}' http://localhost:9130/_/proxy/tenants/diku/modules
